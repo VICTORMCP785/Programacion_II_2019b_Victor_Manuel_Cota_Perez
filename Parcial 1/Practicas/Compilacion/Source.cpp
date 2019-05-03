cced74db9c7008083b15b58020f014f24e7a5d79
VICTORMCP785 <idv18c.vcota@uartesdigitales.edu.mx>
2019-05-02 20:08:08 -0500

    primera entrega parcial 1

---
+++ Parcial 1/Practicas/Compilacion/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//#define INTEGER (x) int x
+using namespace std;
+
+int main()
+{
+	//INTEGER NUM;
+
+	cout << "Hola Mundo" << endl;
+	return 0;
+	cin.get();
+}
+//#include "Header.h"
